e9c063496bce7e3e8924b8bc2ffed0832ac7ff37
Rainy <939029835@qq.com>
2023-11-11 00:31:44 +0800

    Add Color trace support.

--- src/utils/Log.hx
+++ src/utils/Log.hx
@@ -2,10 +2,14 @@ package utils;
 
 class Log {
 	public static function info(...data:String):Void {
-		trace("INFO:", data.toArray());
+		Sys.println('\u001b[34m${data.toArray().join(", ")}');
 	}
 
 	public static function error(...data:String):Void {
-		trace("ERROR:", data.toArray());
+		Sys.println('\u001b[31m${data.toArray().join(", ")}');
+	}
+
+	public static function warring(...data:String):Void {
+		Sys.println('\u001b[33m${data.toArray().join(", ")}');
 	}
 }
