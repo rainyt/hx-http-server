8db16006962ad69328a1bca3c07eb94315da96df
Rainy <939029835@qq.com>
2023-11-11 00:33:01 +0800

    Update to Log api

--- test/Main.hx
+++ test/Main.hx
@@ -1,5 +1,6 @@
 package;
 
+import utils.Log;
 import sys.FileSystem;
 import haxe.io.Path;
 import http.HTTPServer;
@@ -7,13 +8,13 @@ import http.HTTPServer;
 class Main {
 	static function main() {
 		// 启动一个服务器
-		trace("启动服务器");
+		Log.info("启动服务器");
 		var ip:String = "0.0.0.0";
 		var port = 5555;
 		var http = new HTTPServer(ip, port, true);
 		http.onConnectRequest = (http) -> {
-			trace("请求路径：", http.path);
-			trace("请求方法：", http.method);
+			Log.info("请求路径：", http.path);
+			Log.info("请求方法：", http.method);
 			var path = http.getLocalFilePath();
 			if (FileSystem.exists(path)) {
 				http.sendFile(http.path);
