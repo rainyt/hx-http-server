c5eb741e7fdd2fc6a4ea05f4c9182ade94555e29
Rainy <939029835@qq.com>
2023-11-11 16:12:21 +0800

    Update ReflectCustomObject.hx

--- test/ReflectCustomObject.hx
+++ test/ReflectCustomObject.hx
@@ -1,5 +1,5 @@
 /**
- * 自定义反射路由服务
+ * 自定义反射路由服务，每个方法会因为`ReflectRouteMacro`宏，获得一个`http`的对象，通过它进行发送消息。
  */
 @:keep
 @:build(http.macro.ReflectRouteMacro.build())
@@ -8,11 +8,21 @@ class ReflectCustomObject {
 
 	public function new() {}
 
+	/**
+	 * 一个方法调用
+	 * @param value1 
+	 * @param int2 
+	 */
 	public function call1(value1:String, int2:Int):Void {
 		trace("访问到call1接口，并且接受到了数据：", value1, int2);
 		http.send("Accest value is " + value1 + " and int2 is " + int2);
 	}
 
+	/**
+	 * 一个计算除法的简单例子
+	 * @param value1 
+	 * @param value2 
+	 */
 	public function mathDiv(value1:Int, value2:Float):Void {
 		trace("访问到call2接口，并且接受到了数据：", value1, value2);
 		http.send('int1/float=${value1 / value2}\npaths[1]=${http.param.at(1)}');
