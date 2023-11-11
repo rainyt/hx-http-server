0cc0acb6ed9ed9f17b3477829a4ad287ec7b1179
Rainy <939029835@qq.com>
2023-11-11 20:43:43 +0800

    Update ReflectCustomObject.hx

--- test/ReflectCustomObject.hx
+++ test/ReflectCustomObject.hx
@@ -4,8 +4,6 @@
 @:keep
 @:build(http.macro.ReflectRouteMacro.build())
 class ReflectCustomObject {
-	public var custom = 0;
-
 	public function new() {}
 
 	/**
@@ -14,7 +12,6 @@ class ReflectCustomObject {
 	 * @param int2 
 	 */
 	public function call1(value1:String, int2:Int):Void {
-		trace("访问到call1接口，并且接受到了数据：", value1, int2);
 		http.send("Accest value is " + value1 + " and int2 is " + int2);
 	}
 
@@ -24,18 +21,16 @@ class ReflectCustomObject {
 	 * @param value2 
 	 */
 	public function mathDiv(value1:Int, value2:Float):Void {
-		trace("访问到call2接口，并且接受到了数据：", value1, value2);
 		http.send('int1/float=${value1 / value2}\npaths[1]=${http.param.at(1)}');
 	}
 
 	/**
-	 * POST数据获得
+	 * 通过POST数据处理
 	 * @param value 
 	 * @param value2 
 	 */
 	@:post
 	public function postData(value:Int, value2:Int):Void {
-		trace("将获得post数据：", value, value2);
 		http.send("接受到POST数据：value=" + value + "&value2=" + value2);
 	}
 }
