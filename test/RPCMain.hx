6d223230579f11553dc90c3710a6d1bdb037f4c1
Rainy <939029835@qq.com>
2023-11-15 15:54:50 +0800

    Update RPCMain.hx

--- test/RPCMain.hx
+++ test/RPCMain.hx
@@ -5,8 +5,9 @@ import rpc.RPCClient;
  */
 class RPCMain {
 	static function main() {
+		trace(1.1 is Int, 1.1 is Float);
 		var client = new RPCClient("127.0.0.1", 3556);
-		var data = client.callMethod("call", 1, 1.2, "字符串", true, false, {
+		var data = client.callMethod("call", 1, 10.2123123, "字符串", true, false, {
 			key: "hello world"
 		}, ["Goods RPC"]);
 		trace("返回值：", data);
