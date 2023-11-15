6a5c98acb4070da782461ef0000fe14273aa2f12
Rainy <939029835@qq.com>
2023-11-15 13:28:28 +0800

    新增net.SocketServer，可以继承它实现自定义的TCP服务器

--- src/http/HTTPServer.hx
+++ src/http/HTTPServer.hx
@@ -1,26 +1,15 @@
 package http;
 
-import haxe.EntryPoint;
+import net.SocketServer;
 import utils.Log;
 import haxe.Exception;
 import sys.thread.Thread;
-import sys.net.Host;
 import sys.net.Socket;
 
 /**
  * HTTP服务器支持
  */
-class HTTPServer {
-	private var __runing:Bool = true;
-
-	private var __server:Socket;
-
-	private var __ip:String;
-
-	private var __port:Int = 0;
-
-	public var log:Bool = false;
-
+class HTTPServer extends SocketServer {
 	public var webDir:String = "./";
 
 	/**
@@ -29,58 +18,14 @@ class HTTPServer {
 	public var route:HTTPRouteManager;
 
 	/**
-	 * 是否启动SSL
-	 */
-	public var ssl:Bool = false;
-
-	/**
 	 * 构造一个HTTP服务器
 	 * @param ip 
 	 * @param port 
 	 * @param log 
 	 */
 	public function new(ip:String, port:Int, log:Bool = false) {
-		__ip = ip;
-		__port = port;
-		this.log = log;
+		super(ip, port, log);
 		this.route = new HTTPRouteManager(this);
-	}
-
-	/**
-	 * 开始启动HTTP服务器
-	 */
-	public function start():Void {
-		if (ssl) {
-			var sslScoket = new sys.ssl.Socket();
-			__server = sslScoket;
-			// TODO 这里需要设置证书
-			// sslScoket.setCertificate()
-		} else
-			__server = new Socket();
-		var host = new Host(__ip);
-		try {
-			__server.bind(host, __port);
-			__server.listen(1024);
-		} catch (e:Exception) {
-			throw e;
-		}
-		Log.info("excPath", Sys.getCwd());
-		Log.info("listener", __ip + ":" + __port);
-		EntryPoint.addThread(onServerRuning);
-	}
-
-	public function onServerRuning():Void {
-		while (__runing) {
-			var socket = __server.accept();
-			if (socket != null) {
-				try {
-					onConnectClient(socket);
-				} catch (e:Exception) {
-					if (this.log)
-						Log.error(e.message);
-				}
-			}
-		}
 	}
 
 	/**
@@ -99,7 +44,7 @@ class HTTPServer {
 	 * 创建线程管理
 	 * @param client 
 	 */
-	private function onConnectClient(client:Socket):Void {
+	override public function onConnectClient(client:Socket):Void {
 		var head:String = client.input.readLine();
 		if (this.log) {
 			Log.info("connect head:", head);
---
+++ src/net/SocketServer.hx
@@ -0,0 +1,88 @@
+package net;
+
+import haxe.Exception;
+import sys.net.Host;
+import sys.net.Socket;
+import utils.Log;
+import haxe.EntryPoint;
+
+/**
+ * TCP服务器
+ */
+class SocketServer {
+	/**
+	 * 运行状态
+	 */
+	private var __runing:Bool = true;
+
+	/**
+	 * 是否显示日志
+	 */
+	public var log:Bool;
+
+	/**
+	 * 服务器IP
+	 */
+	public var ip:String;
+
+	/**
+	 * 服务器端口
+	 */
+	public var port:Int;
+
+	/**
+	 * 是否启动SSL
+	 */
+	public var ssl:Bool = false;
+
+	private var __server:Socket;
+
+	public function new(ip:String, port:Int, log:Bool = false) {
+		this.log = log;
+		this.ip = ip;
+		this.port = port;
+	}
+
+	public function start():Void {
+		if (ssl) {
+			var sslScoket = new sys.ssl.Socket();
+			__server = sslScoket;
+			// TODO 这里需要设置证书
+			// sslScoket.setCertificate()
+		} else
+			__server = new Socket();
+		var host = new Host(ip);
+		try {
+			__server.bind(host, port);
+			__server.listen(1024);
+		} catch (e:Exception) {
+			throw e;
+		}
+		Log.info("excPath", Sys.getCwd());
+		Log.info("listener", ip + ":" + port);
+		EntryPoint.addThread(onServerRuning);
+	}
+
+	public function onConnectClient(client:Socket):Void {}
+
+	public function onServerRuning():Void {
+		while (__runing) {
+			var socket = __server.accept();
+			if (socket != null) {
+				try {
+					onConnectClient(socket);
+				} catch (e:Exception) {
+					if (this.log)
+						Log.error(e.message);
+				}
+			}
+		}
+	}
+
+	/**
+	 * 停止当前服务器
+	 */
+	public function stop():Void {
+		__runing = false;
+	}
+}
